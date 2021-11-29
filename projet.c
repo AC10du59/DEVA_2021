14dcfc649e35d2e4f4c9cba6e8a5225572438f42
Aurélien <47136367+AC10du59@users.noreply.github.com>
2021-11-29 20:01:08 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*------------------------------------ MENU ----------------------------------*/
+
 const char * MESSAGE_AIDE = "Bienvenue dans le jeu BattleShip !\n\nNous sommes ici pour vous aider et expliquer le jeu.\nLe but de ce jeu est dans un premier temps de placer les bateaux sur le plateau de jeu.\nVous aurez 5 bateaux de tailles différentes à placer. \nVotre adversaire aura exactement les mêmes bateaux que vous.\nUne fois vos bateaux placés, vous devrez couler les bateaux de votre adversaire.\nPour trouver les bateaux, il vous suffira de donner les coordonnées X et Y par rapport au plateau.\nVous essaierez de toucher le bateau l'un aprés l'autre.\nSi vous touchez le bateau, vous serez informé et vous pourrez rejouer immédiatement.\nSi vous ne le touchez pas, ce sera à l'adversaire de jouer.\nLe premier à couler tous les bateaux adverses a gagné.\n\nBonne chance !!!\n\n\n";
 
 void clear(){
@@ -68,7 +70,81 @@ void menu_aide(){
     menu_aide();
 }
 
+
+/*----------------------------- BATAILLE NAVALE ------------------------------*/
+
+#define TAILLE 10
+
+typedef struct une_case {
+    int x;
+    int y;
+} Case;
+
+typedef struct navire {
+    char * nom;
+    int sens; /*0 haut 1 droite 2 bas 3 gauche*/
+    Case premiere_case;
+    int taille;
+} Navire;
+
+/*
+0 ---> rien
+1 ---> tir non touché
+2 ---> bateau touché
+*/
+
+void initialiser_grille(int grille[TAILLE][TAILLE]){
+    for(int i = 0; i < TAILLE; i++){
+        for(int j = 0; j < TAILLE; j++){
+            grille[i][j] = 0;
+        }
+    }
+}
+
+void affiche_grille(int grille[TAILLE][TAILLE]){
+    int i, j;
+    printf("    1 2 3 4 5 6 7 8 9 10\n");
+
+    for(i = 0; i < TAILLE; i++){
+        if(i == 9)printf("%d  ", i+1);
+        else printf(" %d  ", i+1);
+        for(j = 0; j < TAILLE; j++){
+            if(grille[i][j]==0) printf("~ ");
+            else if(grille[i][j]==1) printf("x ");
+            else if(grille[i][j]==2) printf("o ");
+        }
+    printf("\n");
+    }
+}
+
+int est_valide(int x, int y, int grille[TAILLE][TAILLE]){
+    if(x>=0 && x<=10 && y>=0 && y<=10){
+        if(grille[x][y] == 0){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/*
+0 0 0 0 0 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 0 0
+
+*/
+
+
+
+
+
+
+
+/*------------------------------ FONCTION MAIN -------------------------------*/
 int main(int argc, char *argv[]) {  
-    menu();
+    //menu();
+    int plateau1[TAILLE][TAILLE];
+    initialiser_grille(plateau1);
+    affiche_grille(plateau1);
     return 0;
 }
